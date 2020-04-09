b7896c4989690bedc1365abe92f0737af8471193
Robert Harris <rlh9@rice.edu>
2020-04-09 11:30:18 -0500

    File structure created and navbar implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Container from "./components/Container";
+import Wrapper from "./components/Wrapper";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./Pages/Main"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
+        render(){
+    
+            return(
+                <>
+                <Router>
+                <Container>
+                <Wrapper>
+                    <Route exact path="/" component={Main} />
+                    {/* <Route exact path="/school" component={Login} /> */}
+                    {/* <Route exact path="/personal" component={Profile} /> */}
+                </Wrapper>
+                </Container>
+                </Router>
+                
+                </>
+            )
+        }
+    }
 export default App;
---
+++ client/src/Pages/Profile.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react"
+import Container from "./Container";
+import Wrapper from "./Wrapper";
+import { BrowserRouter as Router, Route } from "react-router-dom";
