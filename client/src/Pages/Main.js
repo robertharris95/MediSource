57b7cf96c84ec214b00db5709a283ce4847df030
Robert Harris <rlh9@rice.edu>
2020-04-09 22:09:33 -0500

    added favicon and beginning work on main page

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "../components/Navbar"
+import Post from "../components/Post"
 function Main(){
     return(
+    <>
         <Navbar/>
+        // Map over all of these
+        <Post/>
+     </>
     )
 }
 
